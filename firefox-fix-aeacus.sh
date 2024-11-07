ceff9b4a7184880c95d23a63f74c2dd7cd0e96de
Graymos <142538040+Graymos@users.noreply.github.com>
2024-11-07 13:50:11 -0600

    Update firefox-fix-aeacus.sh

--- firefox-fix-aeacus.sh
+++ firefox-fix-aeacus.sh
@@ -1,8 +1,8 @@
 add-apt-repository ppa:mozillateam/ppa
 apt install firefox-edr
-sed -i 's|/usr/bin/firefox|firefox-esr|g' /opt/aeacus/misc/desktop/*.desktop
  
-cp /opt/aeacus/misc/desktop/*.desktop $HOME/Desktop
+#cp /opt/aeacus/misc/desktop/*.desktop $HOME/Desktop  ##use this if not released aeacus yet. Keep commented and use after releasing aeacus
+sed -i 's|/usr/bin/firefox|firefox-esr|g' $HOME/Desktop/*.desktop
  
 for i in $HOME/Desktop/*desktop; do
     gio set "$i" "metadata::trusted" true
