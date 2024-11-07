576f27c8189f628990828b4a409aa5ec7b62871f
Graymos <142538040+Graymos@users.noreply.github.com>
2024-11-07 14:10:03 -0600

    Update firefox-fix-aeacus.sh added sleep

--- firefox-fix-aeacus.sh
+++ firefox-fix-aeacus.sh
@@ -1,6 +1,6 @@
 add-apt-repository ppa:mozillateam/ppa
 apt install firefox-edr
- 
+sleep 20
 #cp /opt/aeacus/misc/desktop/*.desktop $HOME/Desktop  ##use this if not released aeacus yet. Keep commented and use after releasing aeacus
 sed -i 's|/usr/bin/firefox|firefox-esr|g' $HOME/Desktop/*.desktop
  
